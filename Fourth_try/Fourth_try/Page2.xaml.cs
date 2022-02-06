29d66e6dd61ed6d6e1213f383b9c1b52afb11c29
Γιωργος Κοκκινος <g_red@192.168.1.14>
2022-02-06 18:22:19 +0200

    initial commit

---
+++ Fourth_try/Fourth_try/Page2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Fourth_try
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        public void Registerd(object sender, EventArgs e)
+
+        {
+            
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Fourth_try/Fourth_try/WelcomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Fourth_try
+{
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+        public void Navi(object sender,EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+    }
+}
